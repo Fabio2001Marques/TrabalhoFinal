f34ddb94b1ebcd48f037cbfff12509735c06e849
Fabio Marques <fabio2001marques@gmail.com>
2021-05-26 09:44:28 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipg/trabalho_final/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package pt.ipg.trabalho_final.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
