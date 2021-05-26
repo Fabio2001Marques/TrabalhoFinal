f284a0ce3967f7af2e1986f0ea19fdbd1e9aac0a
Fabio Marques <fabio2001marques@gmail.com>
2021-05-26 09:55:33 +0100

    Criar class para a tabela Vacinas (sem chaves estrangeiras)

---
+++ app/src/main/java/pt/ipg/trabalho_final/TabelaVacinas.kt
@@ -0,0 +1,50 @@
+package pt.ipg.trabalho_final
+
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaVacinas(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+
+    fun cria() {
+        db.execSQL("CREATE TABLE $NOME_Tabela (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $QUANTIDADE INTEGER NOT NULL, $DATA_CHEGADA TEXT NOT NULL)")
+
+    }
+
+
+    // CRUD
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_Tabela, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_Tabela, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_Tabela, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_Tabela, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_Tabela = "Vacinas"
+        const val CAMPO_NOME = "nome"
+        const val QUANTIDADE = "quantidade"
+        const val DATA_CHEGADA = "data_chegada"
+    }
+
+}
