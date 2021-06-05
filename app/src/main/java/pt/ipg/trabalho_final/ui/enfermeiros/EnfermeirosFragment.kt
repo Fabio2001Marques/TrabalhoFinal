6fa7f722ea1d5cf889a5a961c74ab0c4f2113a75
Fabio Marques <fabio2001marques@gmail.com>
2021-06-05 20:33:07 +0100

    Criado o interface da lista Enfermeiros

--- app/src/main/java/pt/ipg/trabalho_final/ui/enfermeiros/EnfermeirosFragment.kt
+++ app/src/main/java/pt/ipg/trabalho_final/ui/enfermeiros/EnfermeirosFragment.kt
@@ -1,4 +1,4 @@
-package pt.ipg.trabalho_final.ui.gallery
+package pt.ipg.trabalho_final.ui.enfermeiros
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import pt.ipg.trabalho_final.R
-import pt.ipg.trabalho_final.databinding.FragmentGalleryBinding
+import pt.ipg.trabalho_final.databinding.FragmentEnfermeirosBinding
 
-class GalleryFragment : Fragment() {
+class EnfermeirosFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private lateinit var enfermeirosViewModel: EnfermeirosViewModel
+    private var _binding: FragmentEnfermeirosBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,16 +24,12 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        enfermeirosViewModel =
+            ViewModelProvider(this).get(EnfermeirosViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentEnfermeirosBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
--- app/src/main/java/pt/ipg/trabalho_final/ui/enfermeiros/EnfermeirosViewModel.kt
+++ app/src/main/java/pt/ipg/trabalho_final/ui/enfermeiros/EnfermeirosViewModel.kt
@@ -1,13 +1,13 @@
-package pt.ipg.trabalho_final.ui.gallery
+package pt.ipg.trabalho_final.ui.enfermeiros
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class EnfermeirosViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is Enfermeiros Fragment"
     }
     val text: LiveData<String> = _text
 }
