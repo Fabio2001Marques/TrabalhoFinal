15f613e3fbb81c9159cb0424e0da4288dc6d3ab7
Fabio Marques <fabio2001marques@gmail.com>
2021-06-17 00:26:51 +0100

    Implementada a função onCreateViewHolder do Adapter Pessoas e Enfermeiros

--- app/src/main/java/pt/ipg/trabalho_final/AdapterEnfermeiros.kt
+++ app/src/main/java/pt/ipg/trabalho_final/AdapterEnfermeiros.kt
@@ -4,14 +4,17 @@ import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import pt.ipg.trabalho_final.ui.enfermeiros.EnfermeirosFragment
 
-class AdapterEnfermeiros (var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterEnfermeiros.ViewHolderEnfermeiro>() {
+class AdapterEnfermeiros (val fragment: EnfermeirosFragment, var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterEnfermeiros.ViewHolderEnfermeiro>() {
     class ViewHolderEnfermeiro(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderEnfermeiro {
-        TODO("Not yet implemented")
+        val itemEnfermeiro = fragment.layoutInflater.inflate(R.layout.item_enfermeiros, parent, false)
+
+        return ViewHolderEnfermeiro(itemEnfermeiro)
     }
 
 
--- app/src/main/java/pt/ipg/trabalho_final/AdapterPessoas.kt
+++ app/src/main/java/pt/ipg/trabalho_final/AdapterPessoas.kt
@@ -4,14 +4,17 @@ import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import pt.ipg.trabalho_final.ui.pessoas.PessoasFragment
 
-class AdapterPessoas (var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterPessoas.ViewHolderPessoa>() {
+class AdapterPessoas (val fragment: PessoasFragment,var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterPessoas.ViewHolderPessoa>() {
     class ViewHolderPessoa(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPessoa {
-        TODO("Not yet implemented")
+        val itemPessoa = fragment.layoutInflater.inflate(R.layout.item_pessoas, parent, false)
+
+        return ViewHolderPessoa(itemPessoa)
     }
 
 
