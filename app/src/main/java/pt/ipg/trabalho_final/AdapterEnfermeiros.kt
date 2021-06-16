c074c8035af53623f7ce70bc173676e5de90bc8d
Fabio Marques <fabio2001marques@gmail.com>
2021-06-17 00:36:05 +0100

    Implementada a função onBindViewHolder do AdapterEnfermeiros

--- app/src/main/java/pt/ipg/trabalho_final/AdapterEnfermeiros.kt
+++ app/src/main/java/pt/ipg/trabalho_final/AdapterEnfermeiros.kt
@@ -3,12 +3,21 @@ package pt.ipg.trabalho_final
 import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pt.ipg.trabalho_final.ui.enfermeiros.EnfermeirosFragment
 
 class AdapterEnfermeiros (val fragment: EnfermeirosFragment, var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterEnfermeiros.ViewHolderEnfermeiro>() {
     class ViewHolderEnfermeiro(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textViewNome = itemView.findViewById<TextView>(R.id.textViewEnfermeirosNome)
+        private val textViewContato = itemView.findViewById<TextView>(R.id.textViewEnfermeirosContato)
+        private val textViewMorada = itemView.findViewById<TextView>(R.id.textViewEnfermeirosMorada)
 
+        fun atualizaEnfermeiro(enfermeiro: Enfermeiro) {
+            textViewNome.text = enfermeiro.nome
+            textViewContato.text = enfermeiro.contacto
+            textViewMorada.text = enfermeiro.morada
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderEnfermeiro {
@@ -19,7 +28,8 @@ class AdapterEnfermeiros (val fragment: EnfermeirosFragment, var cursor : Cursor
 
 
     override fun onBindViewHolder(holder: ViewHolderEnfermeiro, position: Int) {
-        TODO("Not yet implemented")
+        cursor!!.moveToPosition(position)
+        holder.atualizaEnfermeiro(Enfermeiro.fromCursor(cursor!!))
     }
 
 
