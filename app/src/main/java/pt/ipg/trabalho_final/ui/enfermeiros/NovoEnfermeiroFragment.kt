e49398c1c48f9cdb9f8510770b4d23b5a0ef9c3a
Fabio Marques <fabio2001marques@gmail.com>
2021-06-17 22:58:08 +0100

    Criado interface para insirir novos enfermeiros

---
+++ app/src/main/java/pt/ipg/trabalho_final/ui/enfermeiros/NovoEnfermeiroFragment.kt
@@ -0,0 +1,40 @@
+package pt.ipg.trabalho_final.ui.enfermeiros
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import pt.ipg.trabalho_final.databinding.FragmentNovoEnfermeiroBinding
+
+class NovoEnfermeiroFragment : Fragment() {
+    private var _binding: FragmentNovoEnfermeiroBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNovoEnfermeiroBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding.buttonSecond.setOnClickListener {
+        //    findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        //}
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
