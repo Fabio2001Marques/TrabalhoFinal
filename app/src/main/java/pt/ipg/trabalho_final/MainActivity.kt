d9f9174e26488219d1338b901158e9f9d7bc9128
Fabio Marques <fabio2001marques@gmail.com>
2021-07-07 12:39:01 +0100

    Enfermeiros concluído.

--- app/src/main/java/pt/ipg/trabalho_final/MainActivity.kt
+++ app/src/main/java/pt/ipg/trabalho_final/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -39,10 +38,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
